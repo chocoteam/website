624b80f7e2439922c677a3bed632c10d3cfc71af
Charles Prud'homme <charles.prudhomme@imt-atlantique.fr>
2026-05-11 10:50:21 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,11 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# clean up public
+cd public
+find . -maxdepth 1 ! -name '.git' ! -name '.' -exec rm -rf {} +
+
+cd ..
 # Build the project.
 # main website
 hugo --config config.toml 
